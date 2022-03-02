cb84430e74fa52e041205a61152cea81158657b5
Jared <redlaward@gmail.com>
2022-03-02 13:25:56 -0500

    commit tree 3abc77e0678f02fb83871e6cf01e452082b7fbb3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(respose => respose.json())
+.then(json => renderImage(json))
+
+
+
 console.log('%c HI', 'color: firebrick')
